abe1a1fb8c433fb9ea9c1a438235b3f1c801d63f
rarescosmin <rarescosmin97@gmail.com>
2020-12-01 15:03:22 +0200

    updated script to add articles to array

--- web-scraper/web-scraper.py
+++ web-scraper/web-scraper.py
@@ -5,9 +5,19 @@ AUTOVIT_BASE_URL = "https://www.autovit.ro/autoturisme/"
 
 response = requests.get(AUTOVIT_BASE_URL)
 
+articles = []
+
 if response.status_code == 200:
     html = response.text
-    #print(html)
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
+    carDivList = soup.select("div.offers.list")
+    carDivContent = carDivList[0].contents
+    for item in carDivContent:
+        if item != '\n':
+            articles.append(item)
+    
+
+    
+    
+
 
