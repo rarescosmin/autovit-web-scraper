0334fc8368d1d297caca75d73861e492e9374bb7
rarescosmin <rarescosmin97@gmail.com>
2020-12-12 18:01:27 +0200

    added car make and model to array

--- web-scraper/web-scraper.py
+++ web-scraper/web-scraper.py
@@ -3,12 +3,17 @@ from bs4 import BeautifulSoup
 
 AUTOVIT_BASE_URL = "https://www.autovit.ro/autoturisme/"
 
-def getCarArticles(carDivContent):
-    carArticles = []
+def getCars(carDivContent):
+    cars = []
     for item in carDivContent:
         if 'article' in str(item):
-            carArticles.append(item)
-    return carArticles
+            car = {
+                'make': item.contents[3].contents[1].contents[1].text.strip().split()[0],
+                'model': item.contents[3].contents[1].contents[1].text.strip().split()[1]
+            }
+            cars.append(car)
+    return cars
+    
 
 if __name__ == '__main__':
 
@@ -19,7 +24,7 @@ if __name__ == '__main__':
         soup = BeautifulSoup(html, 'html.parser')
         carDivList = soup.select("div.offers.list")
         carDivContent = carDivList[0].contents
-        carArticles = getCarArticles(carDivContent)
+        cars = getCars(carDivContent)
         print ('stop')
     
 
