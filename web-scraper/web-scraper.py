492652888f5cc7899e7d3b47c2cdd9166c7fdef4
rarescosmin <rarescosmin97@gmail.com>
2020-12-12 16:41:54 +0200

    small refactor

--- web-scraper/web-scraper.py
+++ web-scraper/web-scraper.py
@@ -3,21 +3,24 @@ from bs4 import BeautifulSoup
 
 AUTOVIT_BASE_URL = "https://www.autovit.ro/autoturisme/"
 
-response = requests.get(AUTOVIT_BASE_URL)
-
-articles = []
-
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    carDivList = soup.select("div.offers.list")
-    carDivContent = carDivList[0].contents
+def getCarArticles(carDivContent):
+    carArticles = []
     for item in carDivContent:
-        if item != '\n':
-            articles.append(item)
-    
+        if 'article' in str(item):
+            carArticles.append(item)
+    return carArticles
 
-    
+if __name__ == '__main__':
+
+    response = requests.get(AUTOVIT_BASE_URL)
+
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        carDivList = soup.select("div.offers.list")
+        carDivContent = carDivList[0].contents
+        carArticles = getCarArticles(carDivContent)
+        print ('stop')
     
 
 
